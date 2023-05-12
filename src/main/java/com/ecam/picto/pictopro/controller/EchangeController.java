161cae58ed466ef071a0955fc51f8b0659b27d2f
Git-raymond <raymond.saray@yahoo.com>
2023-05-12 16:00:42 +0200

    modif js pour affich mots non catégorisés

--- src/main/java/com/ecam/picto/pictopro/controller/EchangeController.java
+++ src/main/java/com/ecam/picto/pictopro/controller/EchangeController.java
@@ -1,10 +1,5 @@
 package com.ecam.picto.pictopro.controller;
 
-import com.ecam.picto.pictopro.entity.Categorie;
-import com.ecam.picto.pictopro.entity.SousCategorie;
-import com.ecam.picto.pictopro.repository.MotRepository;
-import com.ecam.picto.pictopro.service.CategorieService;
-import com.ecam.picto.pictopro.service.MotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,45 +7,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecam.picto.pictopro.entity.Categorie;
+import com.ecam.picto.pictopro.entity.SousCategorie;
+import com.ecam.picto.pictopro.service.CategorieService;
+import com.ecam.picto.pictopro.service.MotService;
+
 @Controller
 public class EchangeController {
-    @Autowired
-    private MotService motService;
-    @Autowired
-    private CategorieService categorieService;
+	@Autowired
+	private MotService motService;
+	@Autowired
+	private CategorieService categorieService;
 
+	@GetMapping("/echange")
+	public String goEchange(Model model) {
+		model.addAttribute("categories", categorieService.afficherCategories());
+		model.addAttribute("mots", motService.findAll());
+		model.addAttribute("module", "echange");
+		return "echange";
+	}
 
-    @GetMapping("/echange")
-    public String goEchange(Model model) {
-        model.addAttribute("categories",categorieService.afficherCategories());
-        model.addAttribute("mots",motService.findAll());
-        model.addAttribute("module","echange");
-        return "echange";
-    }
+	@RequestMapping("/listeMotsCat/{id}")
+	public String listeParCategorie(Model model, @PathVariable("id") int id) {
 
-    @RequestMapping("/listeMotsCat/{id}")
-    public String listeParCategorie(Model model, @PathVariable("id") int id){
+		Categorie categorie = categorieService.findCategorieById(id);
+		model.addAttribute("categorie", categorie);
 
-        Categorie categorie = categorieService.findCategorieById(id);
-        model.addAttribute("categorie",categorie);
+		if (categorie.getListeMotsParCategorie().size() > 0) {
+			return "/components/listesParCategorie::motItemsCat";
+		} else {
+			return "/components/listesParCategorie::motItemsVide";
+		}
+	}
 
-        if(categorie.getListeMotsParCategorie().size() > 0 && categorie.getListeSousCategorie().size() == 0){
-            return "/components/listesParCategorie::motItemsCat";
-        } else {
-            return "/components/listesParCategorie::motItemsVide";
-        }
-    }
+	@RequestMapping("/listeMotsSousCat/{id}")
+	public String listeParSousCategorie(Model model, @PathVariable("id") int id) {
 
-    @RequestMapping("/listeMotsSousCat/{id}")
-    public  String listeParSousCategorie(Model model, @PathVariable("id") int id){
+		SousCategorie sousCategorie = categorieService.findSousCategorieById(id);
+		model.addAttribute("sousCategorie", sousCategorie);
 
-        SousCategorie sousCategorie = categorieService.findSousCategorieById(id);
-        model.addAttribute("sousCategorie",sousCategorie);
-
-        if(sousCategorie.getListeMotsParSousCategorie().size() > 0) {
-            return "/components/listesParCategorie::motItemsSousCat";
-        } else {
-            return "/components/listesParCategorie::motItemsVide";
-        }
-    }
+		if (sousCategorie.getListeMotsParSousCategorie().size() > 0) {
+			return "/components/listesParCategorie::motItemsSousCat";
+		} else {
+			return "/components/listesParCategorie::motItemsVide";
+		}
+	}
 }
--- src/main/resources/static/js/recupParCategorie.js
+++ src/main/resources/static/js/recupParCategorie.js
@@ -2,56 +2,60 @@
 
 $('#categorie').change(getContent);
 
-function getContent(){
-    var url = "/gestionDesMots/sousCategories/"
-    var id = $("#categorie").val()
-    url = url + id;
-    console.log(id)
+function getContent() {
+	var url = "/gestionDesMots/sousCategories/"
+	var id = $("#categorie").val()
+	url = url + id;
+	console.log(id)
 
-    var tailleListe = $('#categorie option:selected').attr('sousCat')
-    console.log(tailleListe)
+	var tailleListe = $('#categorie option:selected').attr('sousCat')
+	console.log(tailleListe)
 
-    if(tailleListe > 0) {
-         $('#sousCategorie').load(url);
-         $('#divSousCat').removeClass("hidden")
-    } else {
-         $('#divSousCat').removeClass("hidden").addClass("hidden")
-    }
+	if (tailleListe > 0) {
+		$('#sousCategorie').load(url);
+		$('#divSousCat').removeClass("hidden")
+	} else {
+		$('#divSousCat').removeClass("hidden").addClass("hidden")
+	}
 }
 
 
 
 // Afficher les sous-catégories et les mots dans "echange"
 
-$(".categorieItem").click(function(){
+$(".categorieItem").click(function() {
 
-    // Afficher les sous-catégories
-    var url = "/gestionDesMots/sousCategories/"
-    var id = $(this).data("categorie-id");
-    url = url + id;
-    console.log("click sur la catégorie " + id)
-    var tailleListeSousCats = $(this).data('size');
+	// Afficher les sous-catégories
+	var url = "/gestionDesMots/sousCategories/"
+	var id = $(this).data("categorie-id");
+	url = url + id;
+	console.log("click sur la catégorie " + id)
+	var tailleListeSousCats = $(this).data('size');
 
-    if(tailleListeSousCats > 0) {
-             $('#sousCategorieItem').load(url,motsSousCat);
-             $('#divSousCatItem').removeClass("hidden").addClass("d-flex")
+	if (tailleListeSousCats > 0) {
+		$('#sousCategorieItem').load(url, motsSousCat);
+		$('#divSousCatItem').removeClass("hidden").addClass("d-flex")
 
-        } else {
-             $('#divSousCatItem').removeClass("d-flex").addClass("hidden")
-        }
+	} else {
+		$('#divSousCatItem').removeClass("d-flex").addClass("hidden")
+	}
 
-    // Afficher les mots
-    var url2 = "/listeMotsCat/"
-    url2 = url2 + id;
+	// Afficher les mots
+	var url2 = "/listeMotsCat/"
+	url2 = url2 + id;
 
-    var tailleListeMots = $(this).data('size2')
+	var tailleListeMots = $(this).data('size2')
 
-    if(tailleListeMots > 0 && tailleListeSousCats == 0) {
-        $('#motItem').load(url2,dragAndDrop);
-        $('#divMotItem').removeClass("hidden").addClass("d-flex")
-    } else {
-        $('#divMotItem').removeClass("d-flex").addClass("hidden")
-    }
+	if (tailleListeMots > 0 && tailleListeSousCats == 0) {
+		$('#motItem').load(url2, dragAndDrop);
+		$('#divMotItem').removeClass("hidden").addClass("d-flex")
+	} else if (tailleListeMots > 0 && tailleListeSousCats > 0) {
+		$('#motItem').load(url2, dragAndDrop);
+		$('#divMotItem').removeClass("hidden").addClass("d-flex")
+	}
+	else {
+		$('#divMotItem').removeClass("d-flex").addClass("hidden")
+	}
 
 });
 
@@ -61,23 +65,23 @@ $(".categorieItem").click(function(){
 // Au click sur une sous-catégorie, afficher les mots dans "echange"
 // Fonction appelée après le load des sous-catégories
 
-function motsSousCat(){
-  $(".sousCategorieItem").click(function(){
-    var url = "/listeMotsSousCat/"
-        var id = $(this).data("souscategorie-id");
-        url = url + id;
-        console.log("click sur la sous-catégorie " + id)
+function motsSousCat() {
+	$(".sousCategorieItem").click(function() {
+		var url = "/listeMotsSousCat/"
+		var id = $(this).data("souscategorie-id");
+		url = url + id;
+		console.log("click sur la sous-catégorie " + id)
 
-        var tailleListeMots = $(this).data('size')
-        console.log(tailleListeMots)
+		var tailleListeMots = $(this).data('size')
+		console.log(tailleListeMots)
 
-        if(tailleListeMots > 0) {
-            $('#motItem').load(url,dragAndDrop);
-            $('#divMotItem').removeClass("hidden").addClass("d-flex")
-        } else {
-            $('#divMotItem').removeClass("d-flex").addClass("hidden")
-        }
-    })
+		if (tailleListeMots > 0) {
+			$('#motItem').load(url, dragAndDrop);
+			$('#divMotItem').removeClass("hidden").addClass("d-flex")
+		} else {
+			$('#divMotItem').removeClass("d-flex").addClass("hidden")
+		}
+	})
 }
 
 
