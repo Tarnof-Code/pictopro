529c8a993dbf97daa893c350328ac7b99f275fec
Git-raymond <raymond.saray@yahoo.com>
2023-05-22 04:02:32 +0200

    text to speech en FR

--- src/main/resources/static/js/construireUnePhrase.js
+++ src/main/resources/static/js/construireUnePhrase.js
@@ -10,87 +10,88 @@ var phrase;
 
 function updateTableauMots() {
 
-    tableauMots = [];
-    tableauId = [];
-    tableauTags = [];
-    tableauSingulierPluriel = [];
-    tableauFemininMasculin = [];
+	tableauMots = [];
+	tableauId = [];
+	tableauTags = [];
+	tableauSingulierPluriel = [];
+	tableauFemininMasculin = [];
 
-    $(".target .item2").each(function(index) {
-      mot = $(this).data('mot-nom');
-      tags = $(this).data('mot-tags');
-      idMot = $(this).data('mot-id');
+	$(".target .item2").each(function(index) {
+		mot = $(this).data('mot-nom');
+		tags = $(this).data('mot-tags');
+		idMot = $(this).data('mot-id');
 
-      if (index === 0) {
-        mot = mot.charAt(0).toUpperCase() + mot.slice(1);
-      }
+		if (index === 0) {
+			mot = mot.charAt(0).toUpperCase() + mot.slice(1);
+		}
 
-    //  verifClasseGrammaticale();
-    //  tableauMots.push(mot);
+		//  verifClasseGrammaticale();
+		//  tableauMots.push(mot);
 
-   //  tableauTags.push(tags);
+		//  tableauTags.push(tags);
 
-      var singulierPlurielPrecedent = tableauSingulierPluriel[tableauSingulierPluriel.length - 1];
-      var femininMasculinPrecedent = tableauFemininMasculin[tableauFemininMasculin.length - 1];
+		var singulierPlurielPrecedent = tableauSingulierPluriel[tableauSingulierPluriel.length - 1];
+		var femininMasculinPrecedent = tableauFemininMasculin[tableauFemininMasculin.length - 1];
 
-      if(tags.includes("pronom_ou_determinant") && tags.includes("'singulier'")){
-            tableauSingulierPluriel.push("singulier")
-      } else if(tags.includes("'pluriel'") || singulierPlurielPrecedent == "pluriel"){
-         tableauSingulierPluriel.push("pluriel")
-      } else {
-         tableauSingulierPluriel.push("singulier")
-      }
+		if (tags.includes("pronom_ou_determinant") && tags.includes("'singulier'")) {
+			tableauSingulierPluriel.push("singulier")
+		} else if (tags.includes("'pluriel'") || singulierPlurielPrecedent == "pluriel") {
+			tableauSingulierPluriel.push("pluriel")
+		} else {
+			tableauSingulierPluriel.push("singulier")
+		}
 
-      if(tags.includes("'feminin'") || femininMasculinPrecedent == "feminin"){
-         tableauFemininMasculin.push("feminin")
-      } else if (tags.includes("'nom'") && tags.includes("'masculin'")){
-         tableauFemininMasculin.push("masculin")
-      }else {
-         tableauFemininMasculin.push("masculin")
-      }
+		if (tags.includes("'feminin'") || femininMasculinPrecedent == "feminin") {
+			tableauFemininMasculin.push("feminin")
+		} else if (tags.includes("'nom'") && tags.includes("'masculin'")) {
+			tableauFemininMasculin.push("masculin")
+		} else {
+			tableauFemininMasculin.push("masculin")
+		}
 
-        verifClasseGrammaticale();
-        tableauMots.push(mot);
-        tableauId.push(idMot);
-        tableauTags.push(tags);
+		verifClasseGrammaticale();
+		tableauMots.push(mot);
+		tableauId.push(idMot);
+		tableauTags.push(tags);
 
-    });
+	});
 
-    phrase = tableauMots.join(' ');
+	phrase = tableauMots.join(' ');
 
-    $("#contenuPhrase").text(phrase);
+	$("#contenuPhrase").text(phrase);
 }
 
 // Vérification de la nature du mot
-function verifClasseGrammaticale(){
-    if(tags.includes("'verbe'")){
-        conjugaisonPresent();
+function verifClasseGrammaticale() {
+	if (tags.includes("'verbe'")) {
+		conjugaisonPresent();
 
-    } else if (tags.includes("'nom'")) {
-        accordNom();
-    } else if (tags.includes("'adjectif'")) {
-        accordAdjectif();
-    }
+	} else if (tags.includes("'nom'")) {
+		accordNom();
+	} else if (tags.includes("'adjectif'")) {
+		accordAdjectif();
+	}
 }
 
 // Fonction textToSpeech
-function textToSpeech(messageToSpeech){
-    if ('speechSynthesis' in window) {
-      var message = new SpeechSynthesisUtterance();
-      message.text = messageToSpeech;
+function textToSpeech(messageToSpeech) {
+	if ('speechSynthesis' in window) {
+		var message = new SpeechSynthesisUtterance();
+		message.text = messageToSpeech;
 
-      var voices = speechSynthesis.getVoices();
-      message.voice = voices[0];
-      message.rate = 1;
-      message.volume = 0.8;
+		var voices = speechSynthesis.getVoices();
+		// message.voice = voices[0];
+		message.lang = "fr-FR"; // voix en français
+		message.rate = 1;
+		message.volume = 0.8;
 
-      speechSynthesis.speak(message);
-    }
+		speechSynthesis.speak(message);
+	}
 }
 
 // Lecture de la phrase
-$("#lecturePhrase").click(function(){
-    textToSpeech(phrase);
+$("#lecturePhrase").click(function() {
+	textToSpeech(phrase);
 });
 
 
