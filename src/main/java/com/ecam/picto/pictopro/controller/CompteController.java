d0a3e039db7ac9cf046c2c5ec1a88a8d47adebf7
Git-raymond <raymond.saray@yahoo.com>
2023-06-12 09:18:54 +0200

    maj delete compte

--- src/main/java/com/ecam/picto/pictopro/controller/CompteController.java
+++ src/main/java/com/ecam/picto/pictopro/controller/CompteController.java
@@ -3,6 +3,7 @@ package com.ecam.picto.pictopro.controller;
 import com.ecam.picto.pictopro.entity.Professionnel;
 import com.ecam.picto.pictopro.service.ProfessionnelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,16 @@ public class CompteController {
         model.addAttribute("user", user);
         return "compte";
     }
-
+    @GetMapping ("/deleteCompte")
+    public String deleteCompte(Authentication authentication) throws RuntimeException {
+        try {
+            String username = authentication.getName();
+            System.out.println(authentication.getName());
+            professionnelService.deleteUser(username);
+            return "redirect:/suppressionCompteSucces";
+        } catch (RuntimeException e) {
+            return "redirect:/suppressionCompteEchec";
+        }
+    }
 }
 
