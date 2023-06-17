2c3df01d58c43159508e7e689e741cf02ce14bde
Git-raymond <raymond.saray@yahoo.com>
2023-06-17 23:10:38 +0200

    switch activation compte pro par admin

--- src/main/java/com/ecam/picto/pictopro/controller/ProfessionnelController.java
+++ src/main/java/com/ecam/picto/pictopro/controller/ProfessionnelController.java
@@ -1,23 +1,20 @@
 package com.ecam.picto.pictopro.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.ecam.picto.pictopro.entity.DossierMedical;
 import com.ecam.picto.pictopro.entity.Professionnel;
 import com.ecam.picto.pictopro.repository.DossierMedicalRepository;
 import com.ecam.picto.pictopro.repository.ProfessionnelRepository;
 import com.ecam.picto.pictopro.service.ProfessionnelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/consulterLesPros")
@@ -35,14 +32,14 @@ public class ProfessionnelController {
 	@Autowired
 	private ProfessionnelService professionnelService;
 
-	@GetMapping
+	@GetMapping("/listeDesPros")
 	public String consulterLesPros(Model model) {
 		model.addAttribute("professionnels", professionnelService.findAll());
 		model.addAttribute("module", "consulterLesPros");
 		return "consulterLesPros";
 	}
 
-	@GetMapping(path = "/{id}")
+	@GetMapping(path = "/detailsPro/{id}")
 	public String afficherDetailsPro(@PathVariable("id") int id, Model model) {
 		Optional<Professionnel> professionnel = Optional.ofNullable(this.professionnelRepository.findById(id));
 		if (professionnel.isEmpty()) {
@@ -57,4 +54,30 @@ public class ProfessionnelController {
 		model.addAttribute("module", "consulterLesPros");
 		return "detailsPro";
 	}
+
+	@PostMapping("/{id}/activationCompte")
+	public String activationCompte(@PathVariable("id") int id,
+								   @RequestParam("isActive") boolean isActive,
+								   Model model) {
+		Optional<Professionnel> optionalProfessionnel = Optional.ofNullable(professionnelRepository.findById(id));
+		if (optionalProfessionnel.isPresent()) {
+			Professionnel existingProfessionnel = optionalProfessionnel.get();
+			existingProfessionnel.setActive(isActive);
+			professionnelRepository.save(existingProfessionnel);
+			model.addAttribute("message", "Succès de la mise à jour du compte !");
+			model.addAttribute("professionnel", existingProfessionnel);
+			int professionnelId = existingProfessionnel.getId();
+			model.addAttribute("dossierMedicals", dossierMedicalRepository.findAllByProfessionnelId(professionnelId));
+			model.addAttribute("module", "detailsPro");
+		} else {
+			model.addAttribute("message", "Erreur lors de la mise à jour du compte !");
+			model.addAttribute("professionnel", new Professionnel());
+			model.addAttribute("dossierMedicals", new ArrayList<DossierMedical>());
+			model.addAttribute("module", "detailsPro");
+		}
+		return "detailsPro";
+	}
+
+
+
 }
