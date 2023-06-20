618f9afd82c44cef142bde7e79d015826f7ee02e
Git-raymond <raymond.saray@yahoo.com>
2023-06-20 03:15:09 +0200

    page consulterLesQuestions

--- src/main/java/com/ecam/picto/pictopro/controller/QuestionController.java
+++ src/main/java/com/ecam/picto/pictopro/controller/QuestionController.java
@@ -1,15 +1,25 @@
 package com.ecam.picto.pictopro.controller;
 
+import com.ecam.picto.pictopro.entity.Categorie;
+import com.ecam.picto.pictopro.entity.DossierMedical;
+import com.ecam.picto.pictopro.entity.Phrase;
+import com.ecam.picto.pictopro.repository.CategorieRepository;
+import com.ecam.picto.pictopro.repository.DossierMedicalRepository;
+import com.ecam.picto.pictopro.repository.PhraseRepository;
+import com.ecam.picto.pictopro.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.ecam.picto.pictopro.entity.Question;
 import com.ecam.picto.pictopro.service.QuestionService;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/gestionDesQuestions")
@@ -17,6 +27,13 @@ public class QuestionController {
 
 	@Autowired
 	private QuestionService questionService;
+
+	private final QuestionRepository questionRepository;
+	private final CategorieRepository categorieRepository;
+	public QuestionController(QuestionRepository questionRepository, CategorieRepository categorieRepository) {
+		this.questionRepository = questionRepository;
+		this.categorieRepository  = categorieRepository;
+	}
 
 	@GetMapping("/ajouterUneQuestion")
 	public String goQuestion(Model model) {
@@ -34,8 +51,22 @@ public class QuestionController {
 
 	@GetMapping("/consulterLesQuestions")
 	public String consulterLesQuestions(Model model) {
+		model.addAttribute("questions", questionService.afficherQuestions());
 		model.addAttribute("module", "gestionDesQuestions");
-		model.addAttribute("message", "Page Liste des Questions");
 		return "consulterLesQuestions";
 	}
+
+	@GetMapping("/consulterLesQuestions/{id}")
+	public String afficherDetailsQuestion(@PathVariable("id") int id, Model model) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+		}
+		model.addAttribute("question", question.get());
+		List<Categorie> categories = question.get().getCategories();
+		model.addAttribute("categories", categories);
+		model.addAttribute("module", "gestionDesQuestions");
+		return "detailsQuestion";
+	}
+
 }
--- src/main/java/com/ecam/picto/pictopro/service/QuestionService.java
+++ src/main/java/com/ecam/picto/pictopro/service/QuestionService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.ecam.picto.pictopro.entity.Question;
 
-@Service
+
 public interface QuestionService {
 	void ajouterUneQuestion(Question question);
 
